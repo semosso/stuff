51d2497d1759298e8e7fd175532f97b01c861913
Vinicius Azambuja <vnazambuja@gmail.com>
2019-01-13 18:14:49 -0500

    update jar

--- virtualjar.py
+++ virtualjar.py
@@ -5,7 +5,7 @@ import datetime
 log = "../../Google Drive/virtualjar2019.txt"
 today_date = datetime.date.today().strftime(f"%B %d, %Y")
 
-print("You should be grateful for / mindful of all the good things happening around you that you might miss or forget. So what happened today?")
+print("You should be grateful for / mindful of all the good things happening around you that you might miss or forget. So what happened this week?")
 memory = input("> ")
 
 # lns 12, 14 and 17 seem so ugly as they are right now, but I tried to change and couldn't get it to work... so it is what it is for now
