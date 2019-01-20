c4d2199d3d706816fba923aaff2779f464ef3ebf
Vinicius Azambuja <vnazambuja@gmail.com>
2019-01-19 21:31:31 -0500

    great progress on initial classes

--- tennis/tennis.py
+++ tennis/tennis.py
@@ -1,49 +1,82 @@
 from textwrap import dedent
+from random import randint
 
 # class jogador
 class Player(object):
 
     pool = {
-        "Roger Federer": [0, 1, 2],
-        "Rafael Nadal": [0, 1, 2],
-        "Novak Djokovic": [0, 1, 2],
-        "Andy Murray": [0, 1, 2]
+        "Roger Federer": [3, 5, 4],
+        "Rafael Nadal": [4, 3, 5],
+        "Novak Djokovic": [4, 5, 3],
+        "Andy Murray": [4, 4, 4]
     }
 
     def __init__(self, name):
         self.name = name
-        self.strength = Player.pool[f"{name}"][0]
-        self.stamina = Player.pool[f"{name}"][1]
-        self.speed = Player.pool[f"{name}"][2]
+        self.stamina = Player.pool[f"{name}"][0]
+        self.technique = Player.pool[f"{name}"][1]
+        self.spin = Player.pool[f"{name}"][2]
 
-    def power_move(self):
-        print("w/e")
-    
-    def weak_move(self):
-        print("w/e")
+    def court_impact(self, court):
+        self.stamina += court.impact_stamina
+        self.technique += court.impact_technique
+        self.spin += court.impact_spin
 
 # class tipo de quadra
+class Court(object):
 
-# class equipamento
+    slams = {
+        "australian": ["Australian Open", -2, 0, 0],
+        "garros": ["Roland Garros", 0, 0, -2],
+        "wimbledon": ["Wimbledon", -1, -1, 0],
+        "us": ["U.S. Open", 0, -2, 0],
+    }
 
-# class condição climática
+    def __init__(self, tourney):
+        self.name = Court.slams[f"{tourney}"][0]
+        self.impact_stamina = Court.slams[f"{tourney}"][1]
+        self.impact_technique = Court.slams[f"{tourney}"][2]
+        self.impact_spin = Court.slams[f"{tourney}"][3]
+
+# complexity to be added: full scoring system, game by game, set by set
+# class Match(object):
+#     pass
+
+# class Set(Match):
+#     pass
+
+# class Game(Set):
+#     p1_score = 0
+#     p2_score = 0
 
 # class engine
 class Engine(object):
-        
-    def name_input(self, question):
-        print(question)
-        name = input("> ").lower()
-        name = "".join(x for x in Player.pool.keys() if name in x.lower())
+    pass
 
-        while name not in list(Player.pool.keys()):
-            name = self.name_input(dedent(f"""
-            That player is not in the tournament, pick one from the following:
-            {list(Player.pool.keys())}"""))
-        
-        return name
+def name_input(question):
+    print(question)
+    name = input("> ").lower()
+    name = "".join(x for x in Player.pool.keys() if name in x.lower())
 
-name = Engine()
-nome = name.name_input("Escolha")
-jogador = Player(nome)
-print(type(jogador.strength))
+    while name not in list(Player.pool.keys()):
+        name = name_input(f"Pick one from the following: {list(Player.pool.keys())}")
+        
+    return name
+
+nome = name_input("Escolha")
+jogador1 = Player(nome)
+quadra = Court("wimbledon")
+# teste inicio
+print(jogador1.name)
+print(quadra.name, quadra.impact_stamina, quadra.impact_technique, quadra.impact_spin)
+jogador1.court_impact(quadra)
+print(jogador1.pool[nome])
+print(jogador1.name, jogador1.stamina, jogador1.technique, jogador1.spin)
+# teste fim
+
+# simular vários campeonatos e salvar todos num txt para teste
+# atributos de 1 a 10, que impactam a chance de ter um power ou weak move
+# a chance é random, mas os atributos aumentam
+# quadras afetam os atributos
+# quanto mais sets jogados, maior o efeito
+# preciso criar um scoring system
